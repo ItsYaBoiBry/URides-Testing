28d953220f0e83cdd571d8d59585caaf5574f410
ItsYaBoiBry <bryanlow987@gmail.com>
2018-02-06 12:08:26 +0800

    Added Permissions and Uber SDK Config

--- app/src/main/java/uhoo/com/urides/MainActivity.java
+++ app/src/main/java/uhoo/com/urides/MainActivity.java
@@ -1,7 +1,19 @@
 package uhoo.com.urides;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.core.auth.Scope;
+import com.uber.sdk.rides.client.SessionConfiguration;
+
+import java.util.Arrays;
+
+//import Uber sdk
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Assume thisActivity is the current activity
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_CALENDAR);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+
+        //Uber Session Configuration
+        SessionConfiguration config = new SessionConfiguration.Builder()
+                // mandatory
+                .setClientId("r8OaHwB9V7zLxEEZDpE6GTMxAsWwSzpE")
+                // required for enhanced button features
+                .setServerToken("WbnwMcGRd8EjgAc8pOPspusE-db5s6vt4YzuVjqQ")
+                // required for implicit grant authentication
+                .setRedirectUri("https://dashboard.blissbox.asia")
+                // required scope for Ride Request Widget features
+                .setScopes(Arrays.asList(Scope.RIDE_WIDGETS, Scope.HISTORY, Scope.PROFILE, Scope.REQUEST))
+                // optional: set sandbox as operating environment
+                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+                .build();
+
+        //Initialize Session
+        UberSdk.initialize(config);
+
+
+
     }
 }
